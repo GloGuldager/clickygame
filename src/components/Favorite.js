f393c4b9a1a91795706c9ad65153aad9937e3246
gottagoglo <Gloriann@Glorias-iMac.local>
2019-06-16 07:04:31 -0400

    setup files for clickygame

---
+++ src/components/Favorite.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Favorite() {
+  return (
+<ul>
+  <li>chocolate</li>
+  <li>wine</li>
+  </ul>
+
+  )
+}
+
+export default Favorite;
---
+++ src/components/Introduce.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Introduce() {
+  return (
+    <h1>Hello! My name is Gloria</h1>
+
+  )
+}
+
+export default Introduce;
