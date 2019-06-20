63a40d74d1cdcc037270659dd2f9f658a864324f
Gloria Guldager <gloria@Glorias-MacBook-Air.local>
2019-06-20 18:38:32 -0400

    create container file

---
+++ src/components/ImageContainer.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import ImageCard from "./ImageCard/index";
+import Wrapper from "./Wrapper/index";
+import imagecards from "./imagecards.json";
+import Counter from "./components/Counter";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import FooterPage from "./components/FooterPage";
+
+class App extends Component {
+  // Setting this.state.imagecards to the imagecards json array
+  state = {
+    imagecards
+  };
+
+  removeImage = id => {
+    // Filter this.state.imagecards for imagecards with an id not equal to the id being removed
+    const imagecards = this.state.imagecards.filter(image => image.id !== id);
+    // Set this.state.imagecards equal to the new imagecards array
+    this.setState({ imagecards });
+  };
+
+  // Map over this.state.imagecards and render an ImageCard component for each image object
+  render() {
+    return (
+      <>
+      <Navbar/>
+      <Header/>
+      <Counter />
+      <Wrapper>
+        {this.state.imagecards.map(image => (
+          <ImageCard
+            removeImage={this.removeImage}
+            id={image.id}
+            key={image.id}
+            image={image.image}
+          />
+        ))}
+      </Wrapper>
+      <FooterPage />
+      </>
+  );
+  }
+}
+
+export default App;
