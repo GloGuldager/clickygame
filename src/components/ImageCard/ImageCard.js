07cc1ea203af4643ed17c8883b0699b571f922c6
gottagoglo <Gloriann@Glorias-iMac.local>
2019-06-21 11:20:08 -0400

    clean code

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,20 +1,6 @@
 import React from "react";
 import "./style.css";
 
-// function ImageCard(props) {
-//   return (
-//     <div className="card">
-//       <div className="img-container">
-//         <img alt={props.name} src={props.image} />
-//       </div>
-//       {/* <span onClick={() => props.handleIncrement(props.id)}>
-//       </span> */}
-//       <span onClick={props.handleIncrement}>
-//       </span>
-
-//     </div>
-//   );
-// }
 
 const ImageCard = props => (
   <div className="card">
